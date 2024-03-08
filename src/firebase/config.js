c0ccbc2de675ec2d3d2310c927d0511caaebe68a
Raul Madero <r.madero.ramirez@gmail.com>
2024-03-08 12:37:09 -0600

    Migracion local

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5X7aQQlojiqIX7i0Ohz4H06JIR4AtiwQ",
+    authDomain: "paturistear-7269f-9379a.firebaseapp.com",
+    projectId: "paturistear-7269f",
+    storageBucket: "paturistear-7269f.appspot.com",
+    messagingSenderId: "898860195197",
+    appId: "1:898860195197:web:5c6c1ed3aa89827c590014"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
